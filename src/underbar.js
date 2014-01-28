3584a234866aa1e57acee541c1e71de177f1c095
theptrk <patrick.tran06@gmail.com>
2014-01-27 23:14:05 -0800

    Figured out what rules I can break

--- src/underbar.js
+++ src/underbar.js
@@ -8,6 +8,7 @@ var _ = { };
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+    return val
   };
 
   /**
@@ -28,6 +29,15 @@ var _ = { };
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    if (n === undefined){
+      return array[array.length-1]
+    } 
+    else if (n > array.length) {
+      return array 
+    } 
+    else {
+      return array.slice(array.length - n); 
+    }
   };
 
   // Call iterator(value, key, collection) for each element of collection.
